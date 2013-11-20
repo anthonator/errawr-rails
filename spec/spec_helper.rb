c41d0bb471646536729eb61aebad8a2ef7199ac6
Anthony Smith <anthony@sticksnleaves.com>
2013-11-20 18:34:03 -0500

    Removed useless ApplicationController definition.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,8 +25,4 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = 'random'
-  
-  class ApplicationController < ActionController::Base
-    # setup
-  end
 end
