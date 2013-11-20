6e64d7b61d612d3b49fb830850e1e07f5538efd6
Anthony Smith <anthony@sticksnleaves.com>
2013-11-20 18:36:05 -0500

    Include Errawr methods when including Errawr::Rails in controller.

--- lib/errawr/rails.rb
+++ lib/errawr/rails.rb
@@ -1,7 +1,15 @@
-require "errawr/rails/version"
+require 'active_support/rescuable'
+require 'errawr'
+require 'errawr/http'
+
+require 'errawr/rails/renderable'
+require 'errawr/rails/json'
+require 'errawr/rails/version'
 
 module Errawr
   module Rails
-    # Your code goes here...
+    def self.included(base)
+      base.send(:include, Errawr::ClassMethods)
+    end
   end
 end
